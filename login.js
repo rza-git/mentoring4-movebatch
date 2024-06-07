567790abeaf971d9a0bfe8254d1cd51d8f2d92a0
rza-git <123334643+rza-git@users.noreply.github.com>
2024-06-07 20:25:18 +0700

    finishing feature login (#1)
    * finishing feature login
    
    * fixing line
    
    * delete comment todo

--- login.js
+++ login.js
@@ -1,1 +1,10 @@
-// TODO FEATURE LOGIN
+const login = (username, password) => {
+
+    if(username === "admin" && password === "admin") {
+        console.log("Login Successfully")
+    } else {
+        console.log("Wrong Email or Password")
+    }
+}
+
+login("admin", "admin");
