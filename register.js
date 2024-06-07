05df86f3502aaac20e754c053e93dca8dc368559
rza-git <123334643+rza-git@users.noreply.github.com>
2024-06-07 20:23:55 +0700

    finishing feature register (#2)
    * finishing feature register
    
    * fixing line

--- register.js
+++ register.js
@@ -1,1 +1,13 @@
-// TODO FEATURE REGISTER
+const register = (username, password) => {
+
+    // lebih dari 4 karakter
+    if(password.length <= 4) {
+        console.log("Password must be greater than 4 characters")
+        return;
+    }
+
+
+    console.log(`Username with: ${username} registered successfully`);
+}
+
+register("admin", "test");
